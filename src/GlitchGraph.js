10fbfc38b0ac12fe7bc03e55b522ce1455eb02d0
Tristan Simon <simontristandev@gmail.com>
2024-07-30 00:54:49 +0200

    first commit

---
+++ src/GlitchGraph.js
@@ -0,0 +1,15 @@
+const { execSync } = require("child_process");
+
+for (let i = 1; i < 356; i++) {
+    let rnd = Math.ceil(Math.random() * 3);
+
+    for (let j = 1; j < rnd; j++) {
+        execSync("git add .");
+        execSync(
+            `git commit --allow-empty --date="${new Date(Date.now() - i * 24 * 60 * 60 * 1000).toISOString()}" -m "commit"`
+        );
+    }
+    execSync("git push origin main"); // Moved outside of the write callback
+}
+
+
